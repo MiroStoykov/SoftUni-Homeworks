a0e98a994791bd32d5b8f1011cae1aaffdb8779a
MiroStoykov <stanimirstoykov@abv.bg>
2023-10-06 19:33:38 +0300

    homework done

--- Programming Fundamentals with JavaScript/Basic Syntax, Conditional Statements and Loops - Exercise/ThePyramidOfKingDjoser.js
+++ Programming Fundamentals with JavaScript/Basic Syntax, Conditional Statements and Loops - Exercise/ThePyramidOfKingDjoser.js
@@ -5,24 +5,30 @@ function thePyramidOfKingDjoser(base, increment) {
     let lazuli = 0;
     let gold = 0;
     let counter = 1;
+    let steps = Math.ceil(base / 2);
 
-    for (let i = 0; i <= base; i += 2) {
-        stone += (base - i - 2) * (base - i - 2);
+    for (let i = 1; i <= steps - 1; i++) {
+        stone += ((base - i * 2) * (base - i * 2)) * increment;
         if (counter % 5 == 0) {
-            lazuli += (base - i * 4) - 4;
+            lazuli += (((base - (counter - 1) * 2) * 4) - 4) * increment;
         } else {
-            marble += (base - i * 4) - 4;
+            marble += (((base - (counter - 1) * 2) * 4) - 4) * increment;
         }
         counter++;
     }
 
+    if (base % 2 == 0) {
+        gold = 4;
+    } else {
+        gold = 1;
+    }
 
-    console.log(`Stone required: ${stone}`);
-    console.log(`Marble required: ${marble}`);
-    console.log(`Lapis required: ${lazuli}`);
-    console.log(`Gold required: ${gold}`);
-    console.log(`Stone required: ${stone}`);
+    console.log(`Stone required: ${Math.ceil(stone)}`);
+    console.log(`Marble required: ${Math.ceil(marble)}`);
+    console.log(`Lapis Lazuli required: ${Math.ceil(lazuli)}`);
+    console.log(`Gold required: ${Math.ceil(gold)}`);
+    console.log(`Final pyramid height: ${Math.floor((steps) * increment)}`);
 
 }
 
-thePyramidOfKingDjoser(10, 0.75)
+thePyramidOfKingDjoser(11, 1)
